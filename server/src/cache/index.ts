554f2a4472f48f36a67266503322ebe6fac0c369
slimkarboul1 <slim.karboul1@esprit.tn>
2023-02-19 20:27:47 +0100

    fixed redis container connection

--- server/src/cache/index.ts
+++ server/src/cache/index.ts
@@ -3,18 +3,18 @@ import { createClient } from 'redis'
 import Logger from '../core/Logger'
 
 const connectRedis = async () => {
-  console.log('in redis connect method')
   const redisURL = `redis://:${redis.password}@${redis.host}:${redis.port}`
 
   const client = createClient({ url: 'redis://redis:6379' })
   await client.connect()
+  // await client.set('name', 'slim')
+  // const value = await client.get('name')
+  // console.log(value)
 
-  client.on('connect', () => {
-    Logger.info('Cache is connecting'), console.log('onnecting')
-  })
+  client.on('connect', () => Logger.info('Cache is connecting'))
   client.on('ready', () => Logger.info('Cache is ready'))
   client.on('end', () => Logger.info('Cache disconnected'))
-  // client.on('reconnecting', () => Logger.info('Cache is reconnecting'))
+  client.on('reconnecting', () => Logger.info('Cache is reconnecting'))
   client.on('error', (e) => {
     Logger.error(e)
   })
